5809ff56006a51d6cac35c6cb67f811507e61b87
KHUSNIDDIN989 <khusniddin989@gmail.com>
2022-05-11 20:29:52 +0500

    as

---
+++ src/public/js/login.js
@@ -0,0 +1,5 @@
+const form = document.querySelector("form");
+
+form.addEventListener("submit", () => {
+  console.log(username.value);
+});
--- src/server.js
+++ src/server.js
@@ -1,9 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const cookeiPares = require("cookie-parser");
-
-const app = express();
-app.use(cookeiPares());
+const path = require("path");
 
 const routesLogin = require("./routes/login.routes");
 const routesRegstr = require("./routes/regstr.routes");
@@ -11,9 +9,13 @@ const routesUser = require("./routes/user.routes");
 const routesAdmin = require("./routes/admin.routes");
 const routesUpdate = require("./routes/update.routes");
 
+const app = express();
+
+app.use(express.static(path.join(__dirname, "public")));
+
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
-
+app.use(cookeiPares());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(routesLogin);
